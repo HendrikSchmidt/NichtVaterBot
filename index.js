974cdc4a02b86b376e848c9743f2fafe8329762a
HendrikSchmidt <hendriknas@gmail.com>
2018-08-06 18:17:52 +0200

    add all files

---
+++ index.js
@@ -0,0 +1,97 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// let mysql = require('mysql');
+//
+// let connection = mysql.createConnection({
+//     host: 'db746551337.db.1and1.com',
+//     user: 'dbo746551337',
+//     password: '1IchbinaufNichtVaterBot!',
+//     database: 'db746551337'
+// });
+
+app.post('/new-message', function(req, res) {
+    const { message } = req.body;
+
+    console.log(message.chat.id);
+    console.log(message.text);
+
+    // if (message.text === 'addItem') {
+    //
+    //     connection.connect(function(err) {
+    //         if (err) throw err;
+    //         console.log('You are now connected...');
+    //         connection.query('INSERT INTO chats (id, time) VALUES (?, ?)', ['42', '13:00:00'], function(err, result) {
+    //             if (err) throw err;
+    //             else axios.post(
+    //                 'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
+    //                     {
+    //                         chat_id: message.chat.id,
+    //                         text: 'added'
+    //                     }
+    //                 ).then(response => {
+    //                     console.log('Message posted');
+    //                     res.end('ok');
+    //                 }).catch(err => {
+    //                     console.log('Error :', err);
+    //                     res.end('Error :' + err);
+    //                 });
+    //         });
+    //     });
+    // }
+
+    if (!message || message.text.toLowerCase().indexOf('ping') < 0) {
+        return res.end()
+    }
+
+    axios.post(
+        'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
+        {
+            chat_id: message.chat.id,
+            text: 'pong'
+        }
+    ).then(response => {
+        console.log('Message posted');
+        res.end('ok');
+    }).catch(err => {
+        console.log('Error :', err);
+        res.end('Error :' + err);
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Telegram app listening on port 3000!');
+});
+
+let cronJob = require("cron").CronJob;
+
+new cronJob("0 05 13 * * *", function() {
+
+    axios.post(
+        'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
+        {
+            chat_id: 133024044,
+            text: 'Clara wurde erinnert.'
+        }
+    ).then(response => {
+        console.log('Message posted');
+    }).catch(err => {
+        console.log('Error :', err);
+    });
+    axios.post(
+        'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
+        {
+            chat_id: 294184696,
+            text: 'Zeit die Pille zu nehmen :)'
+        }
+    ).then(response => {
+        console.log('Message posted');
+    }).catch(err => {
+        console.log('Error :', err);
+    });
+}, null, true, 'Europe/Berlin');
