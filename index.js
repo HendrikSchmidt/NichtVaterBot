2ffb9ef09b855804760729a158334eab4f6dca9b
HendrikSchmidt <hendriknas@gmail.com>
2018-10-29 19:29:58 +0200

    save new  certs

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let cronJob = require("cron").CronJob;
 const axios = require('axios');
-const PORT = process.env.PORT || 3000
-const path = require('path')
-let request = require('request')
-let cheerio = require('cheerio')
-let fs = require('fs')
-let http = require('http')
-let https = require('https')
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+let request = require('request');
+let cheerio = require('cheerio');
+let fs = require('fs');
+let https = require('https');
 
 const options = {
     key: fs.readFileSync('./ssl/domain-key.txt'),
@@ -25,11 +24,12 @@ app
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('index'))
     .listen(PORT);
-var server = https.createServer(options, app).listen(443);
+
+https.createServer(options, app).listen(443);
 
 const weekdays = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
 
-new cronJob("00 05 13 * * *", function() {
+new cronJob("10 05 13 * * *", function() {
     request({
         method: 'GET',
         url: 'https://www.astroportal.com/tageshoroskope/skorpion/1811/'
@@ -67,21 +67,26 @@ new cronJob("00 05 13 * * *", function() {
                     chat_id: 133024044,
                     text: 'Clara wurde erinnert.'
                 }
-            ).then(response => console.log('Message posted'))
+            ).then(response => {
+                console.log('Message posted');
+                fs.writeFile('./pillTaken', '0', function(err) {
+                    if (err) throw err;
+                });
+            })
             .catch(err => console.log('Error :', err));
         })
         .catch(err => console.log('Error :', err));
     });
 }, null, true, 'Europe/Berlin');
 
-new cronJob("00 * * * * *", function() {
+new cronJob("00 05 * * * *", function() {
     fs.readFile('./pillTaken', 'utf8', function (err, data) {
         if (err) throw err;
         if(data === '0') {
             axios.post(
                 'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
                 {
-                    chat_id: 133024044,
+                    chat_id: 294184696,
                     text: 'Hast du die Pille genommen?'
                 }
             ).then(response => console.log('Message posted'))
@@ -100,27 +105,6 @@ app.post('/new-message', function(req, res) {
         return res.end()
     }
 
-    axios.post(
-        'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
-        {
-            chat_id: message.chat.id,
-            text: message.text
-        }
-    ).then(response => console.log('Message posted'))
-    .catch(err => console.log('Error :', err));
-
-    fs.readFile('./pillTaken', 'utf8', function (err, data) {
-        if (err) throw err;
-        axios.post(
-            'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
-            {
-                chat_id: message.chat.id,
-                text: data
-            }
-        ).then(response => console.log('Message posted'))
-        .catch(err => console.log('Error :', err));
-    });
-
     if (message.text.toLowerCase().indexOf('ping') >= 0) {
         axios.post(
             'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
@@ -128,28 +112,62 @@ app.post('/new-message', function(req, res) {
                 chat_id: message.chat.id,
                 text: 'pong'
             }
-        ).then(response => console.log('Message posted'))
-        .catch(err => console.log('Error :', err));
+        ).then(response => {
+            console.log('Message posted');
+            res.end('ok');
+        }).catch(err => {
+            console.log('Error :', err);
+            res.end('Error :' + err);
+        });
     }
+
+    const answers = ['Gut! 👍', 'Supi 💪', 'Toll! 🤞', 'Weiter so! 🤟', '🤙', '👏'];
+    const answer = answers[Math.floor(Math.random() * answers.length)];
 
     if (message.text.toLowerCase().indexOf('ja') >= 0) {
         fs.readFile('./pillTaken', 'utf8', function (err, data) {
             if (err) throw err;
             if(data === '0') {
-                if (message.chat.id === 133024044) {
+                if (message.chat.id === 294184696) {
                     fs.writeFile('./pillTaken', '1', function(err) {
                         if (err) throw err;
                         axios.post(
                             'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
                             {
                                 chat_id: message.chat.id,
-                                text: 'Gut!'
+                                text: answer
                             }
-                        ).then(response => console.log('Message posted'))
-                        .catch(err => console.log('Error :', err));
+                        ).then(response => {
+                            console.log('Message posted');
+                            res.end('ok');
+                        }).catch(err => {
+                            console.log('Error :', err);
+                            res.end('Error :' + err);
+                        });
                     });
                 }
+            } else {
+                res.end('ok');
             }
+        });
+    }
+
+    if (message.text.toLowerCase().indexOf('debug') >= 0) {
+        fs.readFile('./pillTaken', 'utf8', function (err, data) {
+            if (err) throw err;
+            axios.post(
+                'https://api.telegram.org/bot612610633:AAFVU-joVBwknVNMlxoflcCl_UDAei_YLWM/sendMessage',
+                {
+                    chat_id: message.chat.id,
+                    text: data + '\n' + answer
+                }
+            ).then(response => {
+                console.log('Message posted');
+                res.end('ok');
+            }).catch(err => {
+                console.log('Error :', err);
+                res.end('Error :' + err);
+            });
         });
     }
 });
